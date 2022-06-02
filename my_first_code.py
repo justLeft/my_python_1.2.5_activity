e73b10169131a8231a60b8354c28312f59c59c8b
justLeft <102768156+justLeft@users.noreply.github.com>
2022-06-02 23:15:54 +0200

    Upload activity files

---
+++ my_first_code.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# this is my first line of code
+
+
+# In[1]:
+
+
+print("My name is Lefteris")
+print("The date is 02/06/2022")
+
+
+# In[ ]:
+
+
+# Write a program to analyse real estate data
+
+
+# In[3]:
+
+
+# Property ID and its Sales price
+
+a = 45000
+b = 23400
+c = 67000
+d = 34600
+e = 12900
+
+print (a, b, c, d, e)
+
+
+# In[6]:
+
+
+# Total sales December
+TotalSalesDec = a + b + c + d + e
+print (TotalSalesDec)
+
+
+# In[9]:
+
+
+# Average Sale Price December
+print (TotalSalesDec/5)
+
+
+# In[12]:
+
+
+# Property C Price 
+print (c - 6500)
+
+
+# In[ ]:
+
+
+
+
